afc0e71890aaa3225051de61b0f5af461c850fa6
mrriddwan <83332805+mrriddwan@users.noreply.github.com>
2021-12-09 16:36:53 +0800

    second commit after adjustments

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
-  <h1 c>To Do App</h1>
+
+<div class="container-title">
+  <h1>to do</h1>
+</div>
+
+<div class="container-form">
   <form @submit.prevent="addTodo()">
-    <label class="main-title">New To Do </label>
-    <input v-model="newTodo" name="newTodo" autocomplete="off" />
-    <q-btn @click="addTodo()" color="primary" label="Add To Do" />
+    <div id="input">
+    <q-input 
+    rounded outlined 
+    v-model="newTodo" 
+    name="newTodo" 
+    label="Type here" 
+    autocomplete="off"
+    />
+    </div>
+    <div>
+    <q-btn @click="addTodo()" color="purple" label="Add To Do" />
+    </div>
   </form>
-  <h2>To Do List</h2>
+</div>
+
+<div>
+  <h2>list</h2>
   <ul>
     <li v-for="(todo, index) in todos" :key="index">
-      <span :class="{ done: todo.done }" @click="doneTodo(todo)">{{
-        todo.content
-      }}</span>
-      <button @click="removeTodo(index)">Remove</button>
+      <span :class="{ done: todo.done }" @click="doneTodo(todo)">
+        {{todo.content}}
+      </span> 
+
+      <q-btn push @click="removeTodo(index)" label="Remove" color="red" />
     </li>
+    
   </ul>
-  <h4 v-if="todos.length === 0">Empty list.</h4>
+  <h4 v-if="todos.length === 0">-- Insert item --</h4>
+</div>
+
 </template>
 
 <script>
 import { ref } from 'vue';
+
 export default {
   name: 'App',
   setup() {
@@ -71,17 +93,59 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  background-color: #708CB0;
   height: 100%;
 }
 
 input {
-    margin: 10px;
+    margin: 100px;
 }
 
-.main-title {
-    font: text-weight-bolder;
+html,body {
+  height: 100%;
+  background:  #f5f5f5
 }
+
+form {
+    max-width: 100%;
+    width: 500px;
+    color: white;
+    display: inline-block;
+    margin: 20px;
+}
+
+ul {
+  text-align: center;
+}
+
+li {
+  width: 1000px;
+  font-family: 'Comfortaa', cursive;
+  display: inline-block;
+  font-size: 50px;
+  background-color: rgb(175, 135, 214);
+  padding: 20px;
+  margin: 20px;
+  border-radius: 25px;
+}
+
+h1 {
+  font-family: 'Readex Pro', sans-serif;
+  font-size: 100rem;
+  color: rgb(175, 135, 214);
+  margin: 10px;
+}
+
+h2 {
+  font-family: 'Readex Pro', sans-serif;
+  margin: 5px;
+}
+
+@font-face {
+  font-family: 'Comfortaa', cursive;
+  src: url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@611;700&display=swap');
+  font-family: 'Readex Pro', sans-serif;
+  src: url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@611;700&family=Readex+Pro:wght@700&display=swap');
+}
+
 </style>
+
